d63781fc26a3a4838a2df097b22bbba7b416bfb7
Mario Henrique <mariohenriquezoccante@gmail.com>
2020-10-24 14:54:42 -0300

    Tela Home

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+});
