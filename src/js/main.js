7a62bdba55620ca8c2cfc0cbef488b424d5cbb91
Mario Henrique <mariohenriquezoccante@gmail.com>
2020-10-28 01:44:22 -0300

    Home 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+//Js para o slider.
 $('.slider-principal').slick({
     dots: false,
     infinite: true,
@@ -8,3 +9,116 @@ $('.slider-principal').slick({
     pauseOnFocus: false,
     pauseOnHover: false
 });
+
+
+//Código para fazer o contador dos dados.
+
+/*
+var alreadyVisible = false;
+window.addEventListener('scroll',() => {
+        console.log($('#init-counter').is(":visible"))
+        if($('#init-counter').is(":visible") && !alreadyVisible){
+            alreadyVisible = true;
+            $("#dados").ready(function() {
+                var numero = document.getElementById('numero1');
+                var min = 0;
+                var max = 100;
+                var duração = 1000; // 5 segundos
+            
+                for (var i = min; i <= max; i++) {
+                setTimeout(function(nr) {
+                    numero.innerHTML = nr;
+                }, i * 1000 / max, i);
+                }
+            })
+        }
+})
+
+
+window.addEventListener('scroll',() => {
+    if($('#init-counter').is(":visible"))
+    return (
+        $("#dados").ready(function() {
+            var numero = document.getElementById('numero2');
+            var min = 0;
+            var max = 250;
+            var duração = 1000; // 5 segundos
+        
+            for (var i = min; i <= max; i++) {
+            setTimeout(function(nr) {
+                numero.innerHTML = nr;
+            }, i * 1000 / max, i);
+            }
+        })
+    )
+})
+
+
+
+window.addEventListener('scroll',() => {
+    if($('#init-counter').is(":visible"))
+    return (
+        $("#dados").ready(function() {
+            var numero = document.getElementById('numero3');
+            var min = 0;
+            var max = 800;
+            var duração = 1000; // 5 segundos
+        
+            for (var i = min; i <= max; i++) {
+            setTimeout(function(nr) {
+                numero.innerHTML = nr;
+            }, i * 1000 / max, i);
+            }
+        })
+    )
+})
+
+*/
+
+
+
+
+
+
+    $("#dados").ready(function() {
+        var numero = document.getElementById('numero1');
+        var min = 0;
+        var max = 100;
+        var duração = 1000; // 5 segundos
+    
+        for (var i = min; i <= max; i++) {
+        setTimeout(function(nr) {
+            numero.innerHTML = nr;
+        }, i * 1000 / max, i);
+        }
+    }); 
+    
+    
+
+    $("#dados").ready(function() {
+        var numero = document.getElementById('numero2');
+        var min = 0;
+        var max = 130;
+        var duração = 1000; // 5 segundos
+    
+        for (var i = min; i <= max; i++) {
+        setTimeout(function(nr) {
+            numero.innerHTML = nr;
+        }, i * 1000 / max, i);
+        }
+    }); 
+
+    $("#dados").ready(function() {
+        var numero = document.getElementById('numero3');
+        var min = 0;
+        var max = 1250;
+        var duração = 1000; // 5 segundos
+    
+        for (var i = min; i <= max; i++) {
+        setTimeout(function(nr) {
+            numero.innerHTML = '+ ' + nr + ' ton.';
+        }, i * 1000 / max, i);
+        }
+    }); 
+
+    
